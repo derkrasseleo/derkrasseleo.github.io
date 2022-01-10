e5cf8d45e571ec236e7f2f1b8659bdcc9774da6f
leochras <leo@traussnigg.com>
2022-01-10 06:33:17 +0100

    js Ordner + WebUntisApiTest

---
+++ js/webuntisapitest.js
@@ -0,0 +1,25 @@
+const WebUntis = require('webuntis');
+
+const untis = new WebUntis('htbla_kaindorf', 'traley17', 't&BpMsJd6q#p', 'mese.webuntis.com');
+
+let endTime;
+let day = new Date(2022, 0, 12);
+
+function getSchuleAus(day) {
+
+untis
+    .login()
+    .then(() => {
+        return untis.getOwnTimetableForRange(day, day);
+    })
+    .then((timetable) => {
+        timetable.sort((a, b) => a.startTime - b.startTime);
+        let endTime = WebUntis.convertUntisTime(timetable[timetable.length - 1].endTime, new Date());
+        console.log(endTime);
+    });
+
+}
+
+getSchuleAus(day);
+document.getElementById("schuleAus").innerHTML = "New text!";
+//console.log(endTime);
