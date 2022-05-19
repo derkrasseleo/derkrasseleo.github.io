f82a620dffa52a0bf0e27e7f8eb48cc4bd3f1bd7
leochras <16163571+leochras@users.noreply.github.com>
2022-05-19 14:32:15 +0200

    restructuring/fix links

--- bummerlzaehler/bummerlzaehler.js
+++ bummerlzaehler/bummerlzaehler.js
@@ -3,38 +3,56 @@ var zaehler2 = 0;
 var zaehler3 = 0;
 var zaehler4 = 0;
 
-var name_Spieler1;
-var name_Spieler2;
-var name_Spieler3;
-var name_Spieler4;
-var anzahl_Bummerl;
+var nameSpieler1;
+var nameSpieler2;
+var nameSpieler3;
+var nameSpieler4;
+var anzahlBummerl;
+
+let players=[];
+
+class Player {
+    constructor(name, counter) {
+        this.name = name;
+        this.counter = counter;
+    }
+}
 
 window.onload = function input() {
 
-    var anzahl_Bummerl = prompt("Wie viele Bummerl Maximal ?", "5");
-    var name_Spieler1 = prompt("Spieler 1:", "Spieler1");
-    var name_Spieler2 = prompt("Spieler 2:", "Spieler2");
-    var name_Spieler3 = prompt("Spieler 3:", "Spieler3");
-    var name_Spieler4 = prompt("Spieler 4:", "Spieler4");
+    var playerCount = prompt("Wie viele Spieler?", "4");
 
-    document.getElementById("player1").innerHTML = name_Spieler1;
-    document.getElementById("player2").innerHTML = name_Spieler2;
-    document.getElementById("player3").innerHTML = name_Spieler3;
-    document.getElementById("player4").innerHTML = name_Spieler4;
+    for (let i = 0; i < playerCount; i++) {
+        players.push(prompt(String("Spieler " + i), "Name"));
+        var player = document.createElement("div");
+        player.innerHTML = "<p>players.at(i)</p>";
+        document.body.appendChild(player);
+    }
+    
+/*     var anzahl_Bummerl = prompt("Wie viele Bummerl Maximal ?", "5");
+    var nameSpieler1 = prompt("Spieler 1:", "Spieler1");
+    var nameSpieler2 = prompt("Spieler 2:", "Spieler2");
+    var nameSpieler3 = prompt("Spieler 3:", "Spieler3");
+    var nameSpieler4 = prompt("Spieler 4:", "Spieler4");
+
+    document.getElementById("player1Name").innerHTML = nameSpieler1;
+    document.getElementById("player2Name").innerHTML = nameSpieler2;
+    document.getElementById("player3Name").innerHTML = nameSpieler3;
+    document.getElementById("player4Name").innerHTML = nameSpieler4; */
 }
 
 function addBummerl(spieler) {
-
-    if (spieler == spieler1) {
+    
+    if (spieler == "spieler1") {
         zaehler1++;
         document.getElementById("zaehler1").innerHTML = "Bummerl: " + zaehler1;
-    } else if (spieler == spieler2) {
+    } else if (spieler == "spieler2") {
         zaehler2++;
         document.getElementById("zaehler2").innerHTML = "Bummerl: " + zaehler2;
-    } else if (spieler == spieler3) {
+    } else if (spieler == "spieler3") {
         zaehler3++;
         document.getElementById("zaehler3").innerHTML = "Bummerl: " + zaehler3;
-    } else if (spieler == spieler4) {
+    } else if (spieler == "spieler4") {
         zaehler4++;
         document.getElementById("zaehler4").innerHTML = "Bummerl: " + zaehler4;
     } else {
@@ -70,21 +88,21 @@ function bummerlCheck() {
 
     console.log("BUMMERLCHECK!");
 
-    if (zaehler1 || zaehler2 || zaehler3 >= anzahl_Bummerl) {
+    if (zaehler1 || zaehler2 || zaehler3 >= anzahlBummerl) {
         switch (zaehler1, zaehler2, zaehler3, zaehler4) {
             case zaehler1:
-                alert(name_Spieler1 + "Gewinnt!");
+                alert(nameSpieler1 + "Gewinnt!");
             case zaehler2:
-                alert(name_Spieler2 + "Gewinnt!");
+                alert(nameSpieler2 + "Gewinnt!");
             case zaehler3:
-                alert(name_Spieler3 + "Gewinnt!");
+                alert(nameSpieler3 + "Gewinnt!");
             case zaehler4:
-                alert(name_Spieler4 + "Gewinnt!");
+                alert(nameSpieler4 + "Gewinnt!");
             default:
                 alert("Niemand gewinnt?! (Sollte nicht passieren!)");
         }
     }
 }
 
-console.log(anzahl_Bummerl);
-console.log(name_Spieler1);
+console.log(anzahlBummerl);
+console.log(nameSpieler1);
