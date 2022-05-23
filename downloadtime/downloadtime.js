e453e6096f401269b5a974944cc6b16371b80596
leochras <16163571+leochras@users.noreply.github.com>
2022-05-23 21:31:59 +0200

    downloadtime enhancements

--- downloadtime/downloadtime.js
+++ downloadtime/downloadtime.js
@@ -4,11 +4,12 @@ function wielangenoch(dlSize,dlSpeedMbps) {
     var dlSpeedMbps = 0;
     var currSize = document.getElementById("crSize").value;
 
+console.log(document.getElementById("unit").value);
 
-    if(document.getElementById(unit).getAttribute(value) == "mbit")
+    if(document.getElementById("unit").value == "mbit")
         dlSpeedMbps = document.getElementById("dlSpeed").value;
-    else if (document.getElementById(unit).getAttribute(value) == "mbyte")
-        dlSpeedMbps = document.getElementById("dlSpeed").value;
+    else if (document.getElementById("unit").value == "mbyte")
+        dlSpeedMbps = document.getElementById("dlSpeed").value/8;
     else
         console.log("Error");
 
@@ -17,12 +18,13 @@ function wielangenoch(dlSize,dlSpeedMbps) {
     var timeRemainingMs = (1/((1 / sizeRemaining) * (dlSpeedMbps / 8)))*1000;
     var timeDone = new Date(currentTime.getTime() + timeRemainingMs);
 
-    document.getElementById("remaining").innerHTML =
-        "It will take " + (timeRemainingMs / 1000 / 60 / 60).toFixed(2)  +
-        " hours " + (timeRemainingMs / 1000 / 60).toFixed(2)  + " minutes or " +
-        (timeRemainingMs / 1000).toFixed(2) + " seconds";
+    document.getElementById("hr").toggleAttribute("hidden");
     
+    document.getElementById("remaining").innerHTML =
+        "It will take " + (timeRemainingMs / 1000 / 60 / 60 / 24).toFixed(2) + " days, " +(timeRemainingMs / 1000 / 60 / 60).toFixed(2)  +
+        " hours, " + (timeRemainingMs / 1000 / 60).toFixed(2)  + " minutes or " +
+        (timeRemainingMs / 1000).toFixed(2) + " seconds";
     document.getElementById("current").innerHTML = "Download will be finished at: " +
-        timeDone.toLocaleTimeString();
+        timeDone.toLocaleString();
 }
 
